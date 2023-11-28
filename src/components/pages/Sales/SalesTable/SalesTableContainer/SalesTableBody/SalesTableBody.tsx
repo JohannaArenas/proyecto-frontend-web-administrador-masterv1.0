78f3200f88b017049697760da7251ae870564a04
Johanna_Arenas <89162545+JohannaArenas@users.noreply.github.com>
2023-11-28 09:12:32 -0300

    gestion de actualizaciones

--- src/components/pages/Sales/SalesTable/SalesTableContainer/SalesTableBody/SalesTableBody.tsx
+++ src/components/pages/Sales/SalesTable/SalesTableContainer/SalesTableBody/SalesTableBody.tsx
@@ -1,18 +1,15 @@
-
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { SUB_SECTION_ROUTE_SALES_MODIFY } from "../../../../../Sidebar/subsections";
 import { Sale } from "./interfaces";
 import { SalesTableBodyItem } from "./SalesTableBodyItem/SalesTableBodyItem";
-import "./styles.css"
+import "./styles.css";
 
 export const SalesTableBody = () => {
-
   const [sales, setSales] = useState<Sale[]>();
 
   const getOrdenVentaEstado = () => {
-
     if (localStorage.getItem("token")) {
       const token: string = JSON.stringify(localStorage.getItem("token"));
 
@@ -22,14 +19,16 @@ export const SalesTableBody = () => {
 
       const opcionOrden = window.localStorage.getItem("opcionOrden");
 
-      const URL = "http://localhost:8080/sensor/api/sale-orders/admin?state=" + opcionOrden;
+      const URL =
+        "https://proyecto-backend-web-production.up.railway.app/sensor/api/sale-orders/admin?state=" +
+        opcionOrden;
 
       const config: any = {
         headers: {
           Authorization: "Bearer " + window.localStorage.getItem("token"),
         },
       };
-      
+
       axios
         .get(URL, config)
         .then((res) => {
@@ -41,13 +40,18 @@ export const SalesTableBody = () => {
           console.log(error.res.data.error.message);
         });
     }
-  }
+  };
 
-  useEffect(() => {
-      getOrdenVentaEstado()
-  }, [/*sales*/]);
+  useEffect(
+    () => {
+      getOrdenVentaEstado();
+    },
+    [
+      /*sales*/
+    ]
+  );
 
- /* useEffect(() => {
+  /* useEffect(() => {
     reset(sales);
   }, [sales]);*/
 
@@ -66,4 +70,4 @@ export const SalesTableBody = () => {
       ))}
     </tbody>
   );
-}
+};
